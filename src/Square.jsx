07e627e37bdb6061e99420be0bdbd4a0038daee9
bahnisikha <bahnisikhadhar@gmail.com>
2023-07-21 18:36:23 +0530

    changes made

--- src/Square.jsx
+++ src/Square.jsx
@@ -3,6 +3,7 @@
     return (
       <div
         style={{border: "1px solid black", width: "10vw", height: "10vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "5vw"}}
+        className={value==="X"? "green" : "blue"}
         onClick={onClick}
       >
         {value}
